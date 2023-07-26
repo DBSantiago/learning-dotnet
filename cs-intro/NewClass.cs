82bfa063f65f4f917d01b968982cb27c1f68441a
Santiago De Blasis <deblasissantiago@gmail.com>
2023-07-25 23:16:17 -0300

    Add new class

---
+++ cs-intro/NewClass.cs
@@ -0,0 +1,15 @@
+﻿namespace cs_intro;
+
+public class NewClass
+{
+    public string Name { get; set; }
+    public int Number { get; set; }
+    public int Id { get; private set; }
+
+    public NewClass(string name, int number)
+    {
+        this.Name = name;
+        this.Number = number;
+        this.Id = 0;
+    }
+}
