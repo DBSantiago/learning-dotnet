37ea18b8a6fc0def79896286eddce436e66eed95
Santiago De Blasis <deblasissantiago@gmail.com>
2023-07-25 22:22:47 -0300

    Add new variables

--- cs-intro/Program.cs
+++ cs-intro/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string greetings = "Hello, World!";
+
+Console.WriteLine(greetings);
+
+int number = 7;
+
+Console.WriteLine(number);
+
+var names = new List<string> { "Santiago", "Agustina", "Lucia", "Hugo" };
+
+Console.WriteLine(names);
